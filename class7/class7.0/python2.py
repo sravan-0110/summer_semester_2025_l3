be7b2cfc8372c11d490d6f31c3664e6b5a854f21
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-07 12:32:31 +0530

    add

---
+++ class7/class7.0/python2.py
@@ -0,0 +1,24 @@
+class Node:
+    def _init_(self, value):
+        self.value = value
+        self.children = []
+
+def dfs(root, goal):
+    if not root:
+        return 
+
+    stack = [root]
+
+    while stack:
+        current_node = stack.pop()
+        print(f"Visiting: {current_node.value}")
+
+        if current_node.value == goal:
+            print(f"Goal node '{current_node.value}' found!")
+            return current_node
+
+        # Push children in reverse for left-to-right traversal
+        for child in reversed(current_node.children):
+            stack.append(child)
+
+
