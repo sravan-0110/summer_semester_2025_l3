f046260f190d84d05e03fc14bff1b33603a3b5c3
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-07 12:14:51 +0530

    addition

---
+++ operation.py
@@ -0,0 +1,13 @@
+print("a = sum")
+print("b = difference")
+print("c = product")
+print("o = division")
+
+m = input("Enter your operator: ")
+
+if m == "a":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    y = z + j
+    print(y, "is the sum")
+
