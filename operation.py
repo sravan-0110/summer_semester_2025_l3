22a98702ec78c207d1f50b748e5197ef0c9541af
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-07 12:17:33 +0530

    operation

--- operation.py
+++ operation.py
@@ -10,4 +10,26 @@ if m == "a":
     j = int(input("Enter num 2: "))
     y = z + j
     print(y, "is the sum")
+elif m == "b":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    y = z - j
+    print(y, "is the difference")
 
+elif m == "c":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    y = z * j
+    print(y, "is the product")
+
+elif m == "o":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    if j != 0:
+        y = z / j
+        print(y, "is the division")
+    else:
+        print("Error: Division by zero")
+
+else:
+    print("Error: Invalid operator")
